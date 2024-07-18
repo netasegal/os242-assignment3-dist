4e3ab1ff60116f71ca4d1cc7aba9b0c441e8a7bd
neta-segal <neta.segalbaryo@gmail.com>
2024-07-18 11:01:19 +0000

    task 2 -done

--- user/crypto_srv.c
+++ user/crypto_srv.c
@@ -7,7 +7,7 @@
 #include "kernel/fcntl.h"
 
 #include "kernel/crypto.h"
-
+ 
 int main(void) {
   if(open("console", O_RDWR) < 0){
     mknod("console", CONSOLE, 0);
@@ -18,7 +18,54 @@ int main(void) {
 
   printf("crypto_srv: starting\n");
 
-  // TODO: implement the cryptographic server here
+  if(getpid() != 2) {
+    printf("crypto_srv: must be run as the second process\n");
+    exit(1);
+  }
 
+  while(1){
+    struct crypto_op* va;
+    uint64 size;
+    printf("waiting for request\n");
+    int ret = take_shared_memory_request((void**) &va, &size);
+    if (ret != 0) break;
+    
+    //check size
+    if(size != sizeof(struct crypto_op) + va->key_size + va->data_size){
+      printf("crypto_srv: invalid size\n");
+      exit(1);
+    }
+
+    printf("crypto_srv: received request\n");
+    //check state
+    if(va->state != CRYPTO_OP_STATE_INIT){
+      asm volatile ("fence rw,rw" : : : "memory"); //fence
+      va->state = CRYPTO_OP_STATE_ERROR;
+      printf("crypto_srv: invalid state\n");
+      exit(1);
+    }
+
+    //check type
+    if(va->type != CRYPTO_OP_TYPE_ENCRYPT && va->type != CRYPTO_OP_TYPE_DECRYPT){
+      asm volatile ("fence rw,rw" : : : "memory"); //fence
+      va->state = CRYPTO_OP_STATE_ERROR;
+      printf("crypto_srv: invalid type\n");
+      exit(1);
+    }
+
+    printf("crypto_srv: processing request\n");
+    //decrypt or decrypt
+    for(int i = 0; i < va->data_size; i++){
+        va->payload[va->key_size + i] ^= va->payload[i % va->key_size];
+    }
+
+    printf("crypto_srv: request processed\n");
+
+    //when done decrypting, set state to done
+    asm volatile ("fence rw,rw" : : : "memory"); //fence
+    va->state = CRYPTO_OP_STATE_DONE;
+    remove_shared_memory_request(va, size);
+  }
+  printf("crypto_srv: take_shared_memory_request failed\n");
   exit(0);
 }
